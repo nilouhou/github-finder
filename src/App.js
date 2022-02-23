140a7abba9b7ce2cbf81de86f0827d38805a4e38
Nilou <nilou.houshmand@gmail.com>
2022-02-22 17:06:52 -0800

    router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+import About from "./components/pages/About";
 
 function App() {
 	return (
 		<Router>
 			<Nav />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/notfound" element={<NotFound />} />
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
+
 			<main>content</main>
 			<Footer />
 		</Router>
