c54148c0b73c931841cf6e2626379ed632271d5f
Nilou <nilou.houshmand@gmail.com>
2022-03-02 11:50:30 -0800

    removed title on image

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -44,16 +44,10 @@ const User = () => {
 				</div>
 
 				<div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
-					<div className="custom-card-image mb-6 md:mb-0">
-						<div className="rounded-lg shadow-xl card image-full">
-							<figure>
-								<img src={avatar_url} alt="" />
-							</figure>
-							<div className="card-body justify-end">
-								<h2 className="card-title mb-0">{name}</h2>
-								<p>{login}</p>
-							</div>
-						</div>
+					<div className="card mb-6 md:mb-0">
+						<figure>
+							<img src={avatar_url} alt="" />
+						</figure>
 					</div>
 
 					<div className="col-span-2">
